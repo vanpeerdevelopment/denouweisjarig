bd3148cc184f7362da1fc90dc9e8eec7e5ead3d2
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2021-04-26 23:12:55 +0200

    PWA

--- README.md
+++ README.md
@@ -14,3 +14,7 @@ npx @angular/cli new denouweisjarig
 ```
 
 2. Angular material was added: `npx @angular/cli add @angular/material`
+
+3. Angular PWA was created: `npx @angular/cli add @angular/pwa`
+
+4. PWA icons created using: [maskable.app](https://maskable.app/editor)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import {RoutingModule} from './routing/routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {LayoutModule} from './layout/layout.module';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
 
 @NgModule({
     declarations: [AppComponent],
     imports: [
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: environment.production,
+            registrationStrategy: 'registerWhenStable:30000'
+        }),
         BrowserModule,
         BrowserAnimationsModule,
         LayoutModule,
