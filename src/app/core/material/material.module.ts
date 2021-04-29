e7cf26b2b86eaba17b22716517f2eb1f596071b9
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2021-04-30 00:08:11 +0200

    Tabs op detail pagina

--- src/app/core/material/material.module.ts
+++ src/app/core/material/material.module.ts
@@ -5,6 +5,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
 
 const materialModules = [
     MatToolbarModule,
@@ -13,6 +14,7 @@ const materialModules = [
     MatSidenavModule,
     MatListModule,
     MatCardModule,
+    MatTabsModule,
 ];
 
 @NgModule({
