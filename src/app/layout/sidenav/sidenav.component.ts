6191c5189973d8920a20884ea8c2a7bbd00eadcf
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2021-04-29 23:20:55 +0200

    Scroll to top in sidenav on navigation

--- src/app/layout/sidenav/sidenav.component.ts
+++ src/app/layout/sidenav/sidenav.component.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {MatDrawerMode, MatSidenav} from '@angular/material/sidenav';
+import {MatDrawerMode, MatSidenav, MatSidenavContent} from '@angular/material/sidenav';
 import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {filter, map, tap} from 'rxjs/operators';
 import {BreakpointService} from '../../core/material/breakpoint.service';
-import {Router} from '@angular/router';
+import {NavigationEnd, Router} from '@angular/router';
 import {Fietstocht} from '../../fietstocht/fietsttocht';
 import {FietstochtService} from '../../fietstocht/fietstocht.service';
 
@@ -16,6 +16,8 @@ export class SidenavComponent implements OnInit {
 
     @ViewChild(MatSidenav)
     private matSidenav: MatSidenav | undefined;
+    @ViewChild(MatSidenavContent)
+    private matSidenavContent: MatSidenavContent | undefined;
     sidenavConfig$!: Observable<SidenavConfig>;
     fietstochten!: Fietstocht[];
 
@@ -25,6 +27,10 @@ export class SidenavComponent implements OnInit {
     }
 
     ngOnInit(): void {
+        this.router.events.pipe(
+            filter(event => event instanceof NavigationEnd),
+            tap(() => this.scrollToTop()))
+            .subscribe();
         this.sidenavConfig$ = this.breakpointService
             .isSmallDevice$()
             .pipe(map(isSmall => isSmall ? SidenavConfig.forSmallDevice() : SidenavConfig.forLargeDevice()));
@@ -52,6 +58,10 @@ export class SidenavComponent implements OnInit {
 
     isUrlActive(id: string): boolean {
         return this.router.isActive(`/fietstocht/${id}`, false);
+    }
+
+    private scrollToTop() {
+        this.matSidenavContent?.scrollTo({top: 0, left: 0, behavior: 'auto'});
     }
 }
 
